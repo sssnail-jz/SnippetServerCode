18907c31c7bc0c721667c02f8793be5db0be66c5
郑建文 <1051975984@qq.com>
2022-01-06 00:28:43 +0800

    snippet 控制器里面添加增删改查的请求

--- src/snippet/snippet.controller.ts
+++ src/snippet/snippet.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('snippet')
-export class SnippetController {}
+export class SnippetController {
+  // 获取 snippet 列表
+  @Get('list')
+  snippetList(){
+    return "this is snippet list!"
+  }
+  
+  // 新建 snippet
+  @Post()
+  snippetCreate(@Body() body){
+    return "create successful!"
+  } 
+  
+  // 修改 snippet
+  @Put(':id')
+  snippetPut(@Param('id') id, @Body() body): string{
+    return "modify snippet successful!"
+  }
+
+  // 删除 snippet
+  @Delete(':id')
+  snippetDelete(@Param('id') id: string): string{
+    return "delete Snippet successful!"
+  }
+}
