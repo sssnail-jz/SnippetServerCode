8f83888f3def038a89aa411fa382891badca3392
郑建文 <1051975984@qq.com>
2022-01-06 00:10:16 +0800

    创建了 snippet 控制器

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SnippetController } from './snippet/snippet.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, SnippetController],
   providers: [AppService],
 })
 export class AppModule {}
