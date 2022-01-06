83bcbebec02628d45e0e476bbd78428d6a879787
郑建文 <1051975984@qq.com>
2022-01-06 11:43:46 +0800

    添加文件上传测试

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { SnippetController } from './snippet/snippet.controller';
 import { SnippetService } from './snippet/snippet.service';
 import { TestCookieController } from './testcookie/testcookie.controller';
 import { TestsessionController } from './testsession/testsession.controller';
+import { TestfileuploadController } from './testfileupload/testfileupload.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, SnippetController, TestCookieController, TestsessionController],
+  controllers: [AppController, SnippetController, TestCookieController, TestsessionController, TestfileuploadController],
   providers: [AppService, SnippetService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {join} from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser'
 import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  // 配置模板引擎
+  app.setBaseViewsDir(join(__dirname, '..', 'views')) // 放视图的文件
+  app.setViewEngine('ejs');
 
   // 配置静态资源路径
   app.useStaticAssets('statics'); 
